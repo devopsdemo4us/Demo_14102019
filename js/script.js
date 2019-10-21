810e29dcdea9e48f4fc72005137025822c35d6bb
Lok <lokpavan@hotmail.com>
2019-10-21 23:08:58 +0530

    Test

--- js/script.js
+++ js/script.js
@@ -95,6 +95,9 @@ var updateClock = function()
 
   console.log(messageText); 
   timeEventJS.innerText = messageText;
+  timeEventJS.innerText = messageText;
+  timeEventJS.innerText = messageText;
+  timeEventJS.innerText = messageText;
   lolcatImage.src = image;
   
   showCurrentTime();
@@ -103,6 +106,13 @@ updateClock();
 
 // Getting the clock to increment once a second
 var oneSecond = 1000;
+setInterval( updateClock, oneSecond);
+setInterval( updateClock, oneSecond);
+setInterval( updateClock, oneSecond);setInterval( updateClock, oneSecond);
+setInterval( updateClock, oneSecond);
+setInterval( updateClock, oneSecond);
+setInterval( updateClock, oneSecond);
+setInterval( updateClock, oneSecond);
 setInterval( updateClock, oneSecond);
 
 
